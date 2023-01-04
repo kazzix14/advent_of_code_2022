7fcc32e8e4158e096dc774eacc2676487a1f8be1
Kazuma Murata <kazzix14@gmail.com>
2023-01-05 03:40:01 +0900

    solve day 2 part 2

---
+++ haskell/app/day2-part2.hs
@@ -0,0 +1,72 @@
+import Data.Foldable (find)
+data Shape = Rock | Paper | Scissors deriving (Show, Eq);
+
+instance Ord Shape where
+  compare Rock Paper = LT
+  compare Rock Scissors = GT
+  compare Paper Scissors = LT
+  compare Paper Rock = GT
+  compare Scissors Rock = LT
+  compare Scissors Paper = GT
+  compare _ _ = EQ
+
+shapes :: [Shape]
+shapes = [Rock, Paper, Scissors]
+
+data Result = Lose | Draw | Win deriving (Enum, Show, Eq);
+
+scoreShape :: Shape -> Int
+scoreShape Rock = 1
+scoreShape Paper = 2
+scoreShape Scissors = 3
+
+scoreResult :: Result -> Int
+scoreResult Lose = 0
+scoreResult Draw = 3
+scoreResult Win = 6
+
+myShape :: Shape -> Result -> Shape
+myShape his Lose = do
+  case find (< his) shapes of
+    Just s -> s
+    _ -> error "unreachable"
+myShape his Draw = do
+  case find (his ==) shapes of
+    Just s -> s
+    _ -> error "unreachable"
+myShape his Win = do
+  case find (his <) shapes of
+    Just s -> s
+    _ -> error "unreachable"
+  
+readShape :: Char -> Shape 
+readShape 'A' = Rock
+readShape 'B' = Paper
+readShape 'C' = Scissors
+readShape _ = error "unexpected"
+
+readResult :: Char -> Result
+readResult 'X' = Lose
+readResult 'Y' = Draw
+readResult 'Z' = Win
+readResult _ = error "unexpected"
+
+readLine :: [Char] -> (Shape, Result)
+readLine i = do
+  let his = readShape $ head i  
+  let r = readResult $ i !! 2
+  (his, r)
+
+score :: Shape -> Result -> Int
+score his r = do
+  let m = myShape his r
+  scoreShape m + scoreResult r
+
+main :: IO ()
+main = do
+  ls <- lines <$> readFile "day2.txt"
+  let scores = uncurry score . readLine <$> ls
+  print $ sum scores
+
+
+    
